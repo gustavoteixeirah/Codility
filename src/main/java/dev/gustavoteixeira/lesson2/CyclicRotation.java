a273341b8de0489a52ea03a84c561ee0be458458
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-06-08 00:28:05 -0300

    Solution for CyclicRotation at 100%

--- src/main/java/dev/gustavoteixeira/lesson2/CyclicRotation.java
+++ src/main/java/dev/gustavoteixeira/lesson2/CyclicRotation.java
@@ -5,12 +5,13 @@ public class CyclicRotation {
 
     public static int[] solution(int[] A, int K) {
         var MAX_LENGTH = A.length;
+        if (MAX_LENGTH == 0) return A;
         var array = A.clone();
 
         while (K > 0) {
             var loopArray = array.clone();
 
-            var last = array[MAX_LENGTH-1];
+            var last = array[MAX_LENGTH - 1];
             for (int i = 1; i < MAX_LENGTH; i++) {
                 loopArray[i] = array[i - 1];
             }
