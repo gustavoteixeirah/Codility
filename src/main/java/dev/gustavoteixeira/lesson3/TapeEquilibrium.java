38bf0828098c425d30146989ad9caef40e533483
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-06-08 23:30:38 -0300

    TapeEquilibrium at 100%

--- src/main/java/dev/gustavoteixeira/lesson3/TapeEquilibrium.java
+++ src/main/java/dev/gustavoteixeira/lesson3/TapeEquilibrium.java
@@ -6,48 +6,28 @@ public class TapeEquilibrium {
 
 
     public static int solution(int[] array) {
-        // get the number of possible slices
-        var sliceIndexQuantity = array.length - 1;
-        if (sliceIndexQuantity == 1) return (int) getAbsDiff(array[0], array[1]);
-        // variable that holds the lesser value
-        long minimalDifference = Long.MAX_VALUE;
+        var arrayLength = array.length;
+        if (arrayLength - 1 == 1) return abs(array[0] - array[1]);
 
-        // loop through them
-        for (int i = 1; i < sliceIndexQuantity; i++) {
-            // call the function (calculate left slice)
-            var leftSliceSum = getLeftSliceSum(array, i);
-            // call the function (calculate right slice)
-            var rightSliceSum = getRightSliceSum(array, i);
+        long leftSum = array[0];
 
-            var currentMinimalDifference = getAbsDiff(leftSliceSum, rightSliceSum);
-            if (minimalDifference > currentMinimalDifference)
-                minimalDifference = currentMinimalDifference;
+        long rightSum = 0;
+        for (int j = 1; j < array.length; j++) {
+            rightSum += array[j];
         }
 
+        var minimalDifference = abs(leftSum - rightSum);
+
+        for (int i = 1; i < arrayLength - 1; i++) {
+            leftSum += array[i];
+            rightSum -= array[i];
+            var currentMinimalDifference = abs(leftSum - rightSum);
+            if (currentMinimalDifference < minimalDifference) {
+                minimalDifference = currentMinimalDifference;
+            }
+        }
 
         return (int) minimalDifference;
     }
 
-    private static long getAbsDiff(long x, long y) {
-        return abs(x - y);
-    }
-
-    private static long getRightSliceSum(int[] array, int i) {
-        long sum = 0;
-
-        for (int j = i; j < array.length; j++) {
-            sum += array[j];
-        }
-
-        return sum;
-    }
-
-    private static long getLeftSliceSum(int[] array, int i) {
-        long sum = 0;
-        for (int j = 0; j < i; j++) {
-            sum += array[j];
-        }
-
-        return sum;
-    }
 }
